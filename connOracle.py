77bfef7020d30b3b49d50de67bec5b9954187ba4
zzp <1147916749@qq.com>
2020-08-25 21:08:17 +0800

    21.08

--- connOracle.py
+++ connOracle.py
@@ -11,20 +11,24 @@ db = cx_Oracle.connect('developer/comber@192.168.32.203:1521/bhtest')
 # 输出数据库版本号print db.version
 
 c = db.cursor()   # 获取cursor()
-sql = 'select * from rule where status = 1'
+str = "几十年滴哦环境医嘱就看数据库的"
 
-result = c.execute(sql)  # 默认不返回查询结果集， 返回数据记录数。
+sql = 'select * from rule where “” + str + “” like(%str%)‘
+
+result = c.execute(sql)                             # 默认不返回查询结果集， 返回数据记录数。
 des = str(c.description).decode("string_escape")    # 获取表头
+
 print des
 
-start = "我们213"
+start = "命令:医嘱"
+
 result = start
 
 x = c.execute(sql)  # 使用cursor()操作查询
 
 for i in x.fetchall():       # 展示查询结果，fetchone函数是获得一行结果，fetchall函数是获得所有行结果。均为元组
-    key = str(i[1]).decode("string_escape")
-    value = str(i[2]).decode("string_escape")
+    key = str(i[2]).decode("string_escape")
+    value = str(i[1]).decode("string_escape")
     res = result.replace(key, value)
     result = res
 
--- socketClient.py
+++ socketClient.py
@@ -11,29 +11,15 @@ sys.setdefaultencoding('utf-8')
 HOST = '192.168.32.109'
 PORT = 50088
 ADDR = (HOST, PORT)
-
 s = socket(AF_INET, SOCK_STREAM)
-s.bind(ADDR)
-s.listen(5)
-
-# def logFileRead(logFile):
-#     '''
-#     Read logFile by line
-#     return List
-#     '''
-#     logFileDealList = []
-#     with open(logFile, 'r') as logFileContent:
-#         for line in logFileContent.readlines():
-#             logFileDealList.append(line)
-#     return logFileDealList
+s.connect(ADDR)
 
 
 def def_socket_thread():
     try:
         while True:
             c, addr = s.accept()
-            resu = "客户端:" + addr[0] + ",连接成功！"
-            print resu
+            print "服务端:", addr[0], ",连接成功！"
             t1 = threading.Thread(target=write_from_client, args=(c, addr))
             t1.start()
             t2 = threading.Thread(target=read_from_client, args=(c, addr))
@@ -41,22 +27,17 @@ def def_socket_thread():
     except IOError as e:
         print("出现错误:", e.strerror)
     s.close()
-    print '连接断开!'
+    print('连接断开!')
 
 
 def read_from_client(c, addr):
     while True:
         content = c.recv(1024).decode('gbk')
         if content == "":
-            resu = "客户端:" + addr[0] + ",断开连接！"
-            print resu
+            print "服务端:", addr[0], ",断开连接！"
             break
         else:
-            resu = addr[0] + ":" + content
-            print resu
-        # else:
-        #     msg = "接收到了:" + content
-        #     c.send(msg.encode('gbk'))
+            print addr[0] + ":" + content
 
 
 def write_from_client(c, addr):
