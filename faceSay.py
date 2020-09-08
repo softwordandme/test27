bf42a7cd41b4f37cb691ffd1e054ad736cb64c8a
zzp <1147916749@qq.com>
2020-09-08 14:06:51 +0800

    人脸识别

---
+++ faceSay.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import base64
+
+import requests
+
+
+
+# 人脸图片转base64
+def imgtobase64():
+    with open(".\images\\zt005.jpg", "rb") as f:  # 图片路径转为二进制格式
+        base64_data = base64.b64encode(f.read())  # 使用base64进行加密
+    return base64_data  # str(base64_data, 'utf-8')
+
+
+# 调用百度人脸搜索API进行查找
+def facesearch():
+    access_token = ""
+    sendMessage = ""
+    imageBase64 = imgtobase64()
+    ak = "flOXbwIWB6FxfBECn0w3Y3F3"
+    sk = "AnNRfoWYYF1BfuTCp1SoZ1xrom0NNNbE"
+    # client_id 为官网获取的AK， client_secret 为官网获取的SK
+    host = "https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=" + ak + "&client_secret=" + sk
+    akResponse = requests.get(host)
+    if akResponse:
+        access_token = akResponse.json()["access_token"]
+    request_url = "https://aip.baidubce.com/rest/2.0/face/v3/search"
+    params = "{\"image\":\"" + imageBase64 + "\",\"image_type\":\"BASE64\",\"group_id_list\":\"faceTest\",\"quality_control\":\"LOW\",\"liveness_control\":\"NORMAL\"}"
+    request_url = request_url + "?access_token=" + access_token
+    headers = {'content-type': 'application/json'}
+    response = requests.post(request_url, data=params, headers=headers)
+    if response:
+        rdata = response.json()
+        userList = rdata["result"]["user_list"]
+        if len(userList) > 0 or userList != "" :
+            userScore = rdata["result"]["user_list"][0]["score"]
+            userId = rdata["result"]["user_list"][0]["user_id"]
+            if userScore > 80 :   #匹配度大于80时
+                sendMessage = "{\"code\":11,\"message\":{\"uid\":\"" + userId + "\",\"image\":\"\"}}"
+        else:
+            sendMessage = "{\"code\":11,\"message\":{\"uid\":\"001\",\"image\":\"" + imageBase64 + "\"}}"
+        return sendMessage
+
+
+if __name__ == '__main__':
+    facesearch()
--- socketService.py
+++ socketService.py
@@ -5,11 +5,13 @@ from socket import *
 import threading
 import inspect
 import sys
+import faceSay
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-HOST = '192.168.32.109'
-PORT = 50088
+HOST = '192.168.72.161'
+PORT = 12221
 ADDR = (HOST, PORT)
 
 s = socket(AF_INET, SOCK_STREAM)
@@ -35,8 +37,8 @@ def def_socket_thread():
             resu = "客户端:" + addr[0] + ",连接成功！"
             print resu
             t1 = threading.Thread(target=write_from_client, args=(c, addr))
-            t1.start()
             t2 = threading.Thread(target=read_from_client, args=(c, addr))
+            t1.start()
             t2.start()
     except IOError as e:
         print("出现错误:", e.strerror)
@@ -45,12 +47,15 @@ def def_socket_thread():
 
 
 def read_from_client(c, addr):
+    resu = ""
     while True:
         content = c.recv(1024).decode('gbk')
         if content == "":
             resu = "客户端:" + addr[0] + ",断开连接！"
             print resu
             break
+        elif content == "10":
+            print "开始人脸识别:" + resu
         else:
             resu = addr[0] + ":" + content
             print resu
@@ -61,10 +66,13 @@ def read_from_client(c, addr):
 
 def write_from_client(c, addr):
     while True:
-        data = raw_input()
-        if data != "":
-            msg = data.encode('gbk')
-            c.send(msg)
+        start = raw_input()
+        if start == "start":
+            data = faceSay.facesearch()
+            print data
+            if data != "":
+                msg = data.encode('gbk')
+                c.send(msg)
 
 
 if __name__ == '__main__':
